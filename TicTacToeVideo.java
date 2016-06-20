d75216abf739b9064ee43bbf6b89ac052ec444a0
LesterCarter <lestercarter97@gmail.com>
2016-06-20 03:15:18 -0400

    The Finished Code

--- TicTacToeVideo.java
+++ TicTacToeVideo.java
@@ -14,9 +14,7 @@ public class TicTacToeVideo {
     
     
     
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) throws InterruptedException
     {
         // TODO code application logic here
@@ -35,6 +33,7 @@ public class TicTacToeVideo {
         
         String first = JOptionPane.showInputDialog("Who goes first?? the Human(Enter H) or Computer (Enter C)");
         
+        //<editor-fold defaultstate="collapsed" desc="Computer Goes First">
         if(first.equals("C"))
         {
             System.out.println("Computer goes first");
@@ -55,48 +54,48 @@ public class TicTacToeVideo {
                 String move = JOptionPane.showInputDialog("enter your next move");
                 
                 //<editor-fold defaultstate="collapsed" desc="moves of the Human">
-                    if(move.equals("00") && board[0][0] == '-')
-                    {
-                        board[0][0] = 'O';
-                    }
-                    else if(move.equals("01") && board[0][1] == '-')
-                    {
-                        board[0][1] = 'O';
-                    }
-                    else if(move.equals("02") && board[0][2] == '-')
-                    {
-                        board[0][2] = 'O';
-                    }
-                    else if(move.equals("10") && board[1][0] == '-')
-                    {
-                        board[1][0] = 'O';
-                    }
-                    else if(move.equals("11") && board[1][1] == '-')
-                    {
-                        board[1][1] = 'O';
-                    }
-                    else if(move.equals("12") && board[1][2] == '-')
-                    {
-                        board[1][2] = 'O';
-                    }
-                    else if(move.equals("20") && board[2][0] == '-')
-                    {
-                        board[2][0] = 'O';
-                    }
-                    else if(move.equals("21") && board[2][1] == '-')
-                    {
-                        board[2][1] = 'O';
-                    }
-                    else if(move.equals("22") && board[2][2] == '-')
-                    {
-                        board[2][2] = 'O';
-                    }
-                    else
-                    {
-                        System.out.println("That is not a valid move");
-                    }
-    //</editor-fold>
-
+                if(move.equals("00") && board[0][0] == '-')
+                {
+                    board[0][0] = 'O';
+                }
+                else if(move.equals("01") && board[0][1] == '-')
+                {
+                    board[0][1] = 'O';
+                }
+                else if(move.equals("02") && board[0][2] == '-')
+                {
+                    board[0][2] = 'O';
+                }
+                else if(move.equals("10") && board[1][0] == '-')
+                {
+                    board[1][0] = 'O';
+                }
+                else if(move.equals("11") && board[1][1] == '-')
+                {
+                    board[1][1] = 'O';
+                }
+                else if(move.equals("12") && board[1][2] == '-')
+                {
+                    board[1][2] = 'O';
+                }
+                else if(move.equals("20") && board[2][0] == '-')
+                {
+                    board[2][0] = 'O';
+                }
+                else if(move.equals("21") && board[2][1] == '-')
+                {
+                    board[2][1] = 'O';
+                }
+                else if(move.equals("22") && board[2][2] == '-')
+                {
+                    board[2][2] = 'O';
+                }
+                else
+                {
+                    System.out.println("That is not a valid move");
+                }
+                //</editor-fold>
+                
                 System.out.println();
                 move();
                 
@@ -115,60 +114,80 @@ public class TicTacToeVideo {
                 System.out.println("Game is a draw");
             }
             
-            
-            
-
         }
+//</editor-fold>
+        
+        
         else if(first.equals("H"))
         {
             
-            System.out.println("Human goes first");
+            System.out.println("Human goes first");     
+            String move = JOptionPane.showInputDialog("enter your next move");
+            //<editor-fold defaultstate="collapsed" desc="moves of the Human">
+            if(move.equals("00") && board[0][0] == '-')
+            {
+                board[0][0] = 'O';
+            }
+            else if(move.equals("01") && board[0][1] == '-')
+            {
+                board[0][1] = 'O';
+            }
+            else if(move.equals("02") && board[0][2] == '-')
+            {
+                board[0][2] = 'O';
+            }
+            else if(move.equals("10") && board[1][0] == '-')
+            {
+                board[1][0] = 'O';
+            }
+            else if(move.equals("11") && board[1][1] == '-')
+            {
+                board[1][1] = 'O';
+            }
+            else if(move.equals("12") && board[1][2] == '-')
+            {
+                board[1][2] = 'O';
+            }
+            else if(move.equals("20") && board[2][0] == '-')
+            {
+                board[2][0] = 'O';
+            }
+            else if(move.equals("21") && board[2][1] == '-')
+            {
+                board[2][1] = 'O';
+            }
+            else if(move.equals("22") && board[2][2] == '-')
+            {
+                board[2][2] = 'O';
+            }
+            else
+            {
+                System.out.println("That is not a valid move");
+            }
+            //</editor-fold>
+
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    System.out.print(board[i][j] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+            //</editor-fold>
+            
             while(!end)
             {
-                String move = JOptionPane.showInputDialog("enter your next move");
+                hMove();
                 
-                //<editor-fold defaultstate="collapsed" desc="moves of the Human">
-                    if(move.equals("00") && board[0][0] == '-')
-                    {
-                        board[0][0] = 'O';
-                    }
-                    else if(move.equals("01") && board[0][1] == '-')
-                    {
-                        board[0][1] = 'O';
-                    }
-                    else if(move.equals("02") && board[0][2] == '-')
-                    {
-                        board[0][2] = 'O';
-                    }
-                    else if(move.equals("10") && board[1][0] == '-')
-                    {
-                        board[1][0] = 'O';
-                    }
-                    else if(move.equals("11") && board[1][1] == '-')
-                    {
-                        board[1][1] = 'O';
-                    }
-                    else if(move.equals("12") && board[1][2] == '-')
-                    {
-                        board[1][2] = 'O';
-                    }
-                    else if(move.equals("20") && board[2][0] == '-')
-                    {
-                        board[2][0] = 'O';
-                    }
-                    else if(move.equals("21") && board[2][1] == '-')
-                    {
-                        board[2][1] = 'O';
-                    }
-                    else if(move.equals("22") && board[2][2] == '-')
-                    {
-                        board[2][2] = 'O';
-                    }
-                    else
-                    {
-                        System.out.println("That is not a valid move");
-                    }
-    //</editor-fold>
+                end = won();
+                
+                if(end)
+                {
+                    break;
+                }
+                
                 for(int i = 0; i < 3; i++)
                 {
                     for(int j = 0; j < 3; j++)
@@ -177,23 +196,81 @@ public class TicTacToeVideo {
                     }
                     System.out.println();
                 }
+                
+                move = JOptionPane.showInputDialog("enter your next move");
+
+                //<editor-fold defaultstate="collapsed" desc="moves of the Human">
+                if(move.equals("00") && board[0][0] == '-')
+                {
+                    board[0][0] = 'O';
+                }
+                else if(move.equals("01") && board[0][1] == '-')
+                {
+                    board[0][1] = 'O';
+                }
+                else if(move.equals("02") && board[0][2] == '-')
+                {
+                    board[0][2] = 'O';
+                }
+                else if(move.equals("10") && board[1][0] == '-')
+                {
+                    board[1][0] = 'O';
+                }
+                else if(move.equals("11") && board[1][1] == '-')
+                {
+                    board[1][1] = 'O';
+                }
+                else if(move.equals("12") && board[1][2] == '-')
+                {
+                    board[1][2] = 'O';
+                }
+                else if(move.equals("20") && board[2][0] == '-')
+                {
+                    board[2][0] = 'O';
+                }
+                else if(move.equals("21") && board[2][1] == '-')
+                {
+                    board[2][1] = 'O';
+                }
+                else if(move.equals("22") && board[2][2] == '-')
+                {
+                    board[2][2] = 'O';
+                }
+                else
+                {
+                    System.out.println("That is not a valid move");
+                }
+                //</editor-fold>
+
                 System.out.println();
-                move();
+                                
+                end = won();
                 
             }
             
-            if(win == 'o')
+            
+            for(int i = 0; i < 3; i++)
             {
-                System.out.println("Human Won");
+                for(int j = 0; j < 3; j++)
+                {
+                    System.out.print(board[i][j] + " ");
+                }
+                System.out.println();
             }
-            else if(win == 'X')
+            
+            if(win == 'X')
             {
                 System.out.println("Computer Won");
             }
-            else if(win == 'd' || win == 'D')
+            else if(win == 'o')
             {
-                System.out.println("Game is a draw");
+                System.out.println("Human Won");
             }
+            else if(win == 'd')
+            {
+                System.out.println("Game is a Draw");
+            }
+            
         }
         else
         {
@@ -876,4 +953,242 @@ public class TicTacToeVideo {
 
         return won;
     }
+    
+    public static void hMove()
+    {
+        //<editor-fold defaultstate="collapsed" desc="first move of the computer">
+        if(counter == 1)
+        {
+            //if the human moved in the center
+            if(board[1][1] == 'O')
+            {
+                Random rand = new Random();
+                int f = rand.nextInt(4) + 1;
+                if(f == 1)
+                {
+                    //then it is 00
+                    board[0][0] = 'X';
+                    firstMove = "00";
+                    //System.out.println("CHECK");
+                }
+                else if(f == 2)
+                {
+                    //then it is 00
+                    board[0][2] = 'X';
+                    firstMove = "02";
+                    //System.out.println("CHECK");
+                }
+                else if(f == 3)
+                {
+                    //then it is 00
+                    board[2][0] = 'X';
+                    firstMove = "20";
+                    //System.out.println("CHECK");
+                }
+                else if(f == 4)
+                {
+                    //then it is 00
+                    board[2][2] = 'X';
+                    firstMove = "22";
+                    //System.out.println("CHECK");
+                }
+                else
+                {
+                    //then it is 00
+                    board[2][2] = 'X';
+                    firstMove = "22";
+                    //System.out.println("CHECK");
+                }
+            }
+            else
+            {
+                board[1][1] = 'X';
+                firstMove = "11";
+            }
+            
+        }
+//</editor-fold>
+
+        //<editor-fold defaultstate="collapsed" desc="second move of the computer">
+if(counter == 2)
+{
+    int second[] = thirdMove();
+    //if there is no defensice move
+    if(second[0] == 5)
+    {
+        if(board[1][1] != 'X')
+        {
+            if(firstMove == "00" && board[0][2] == '-')
+            {
+                board[0][2] = 'X';
+            }
+            else if(firstMove == "00" && board[2][0] == '-')
+            {
+                board[2][0] = 'X';
+            }
+            else if(firstMove == "02" && board[0][0] == '-')
+            {
+                board[0][0] = 'X';
+            }
+            else if(firstMove == "02" && board[2][2] == '-')
+            {
+                board[2][2] = 'X';
+            }
+            else if(firstMove == "20" && board[0][0] == '-')
+            {
+                board[0][0] = 'X';
+            }
+            else if(firstMove == "20" && board[2][2] == '-')
+            {
+                board[2][2] = 'X';
+            }
+            else if(firstMove == "22" && board[2][0] == '-')
+            {
+                board[2][0] = 'X';
+            }
+            else if(firstMove == "22" && board[0][2] == '-')
+            {
+                board[0][2] = 'X';
+            }
+        }
+        else
+        {
+            if(board[0][0] == '-')
+            {
+                board[0][0] = 'X';
+            }
+            else if(board[0][2] == '-')
+            {
+                board[0][2] = 'X';
+            }
+            else if(board[2][0] == '-')
+            {
+                board[2][0] = 'X';
+            }
+            else if(board[2][2] == '-')
+            {
+                board[2][2] = 'X';
+            }
+        }
+        
+        
+        
+    }
+    //if there is a defensive move
+    else
+    {
+        board[second[0]][second[1]] = 'X';
+    }
+    
+}
+//</editor-fold>
+
+        //<editor-fold defaultstate="collapsed" desc="third ">
+if(counter == 3)
+{
+    if(!end)
+    {
+        int second[] = thirdMove();
+        if(second[0] != 5)
+        {
+            //System.out.println("CHECK");
+            board[second[0]][second[1]] = 'X';
+        }
+        else
+        {
+            int secondMy[] = thirdMyMove();
+            if(secondMy[0] != 5)
+            {
+                board[secondMy[0]][secondMy[1]] = 'X';
+            }
+            else
+            {
+                secondMy = noOtherOption();
+                board[secondMy[0]][secondMy[1]] = 'X';
+            }
+        }
+    }
+    if(!end)
+    {
+        end = true;
+        win = 'd';
+    }
+    end = won();
+}
+//</editor-fold>
+
+        //<editor-fold defaultstate="collapsed" desc="fourth">
+        if(counter == 4)
+        {
+
+            if(!end)
+            {
+                int second[] = thirdMove();
+                if(second[0] != 5)
+                {
+                    //System.out.println("CHECK");
+                    board[second[0]][second[1]] = 'X';
+                }
+                else
+                {
+                    int secondMy[] = thirdMyMove();
+                    if(secondMy[0] != 5)
+                    {
+                        board[secondMy[0]][secondMy[1]] = 'X';
+                    }
+                    else
+                    {
+                        secondMy = noOtherOption();
+                        board[secondMy[0]][secondMy[1]] = 'X';
+                    }
+                }
+            }
+            if(!end)
+            {
+                end = true;
+                win = 'd';
+            }
+            won();
+        }
+//</editor-fold>
+        
+        //<editor-fold defaultstate="collapsed" desc="fifth">
+        if(counter == 5)
+        {
+            
+            
+            won();
+            
+            if(!end)
+            {
+                int second[] = thirdMove();
+                if(second[0] != 5)
+                {
+                    //System.out.println("CHECK");
+                    board[second[0]][second[1]] = 'X';
+                }
+                else
+                {
+                    int secondMy[] = thirdMyMove();
+                    if(secondMy[0] != 5)
+                    {
+                        board[secondMy[0]][secondMy[1]] = 'X';
+                    }
+                    else
+                    {
+                        secondMy = noOtherOption();
+                        board[secondMy[0]][secondMy[1]] = 'X';
+                    }
+                }
+            }
+
+            
+        }
+//</editor-fold>
+
+        counter++;
+    }
+    
+    
+    
 }
